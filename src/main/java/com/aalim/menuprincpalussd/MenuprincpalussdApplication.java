78bac7df812c78d9a9281b9739091015d0b7cd89
dialloalim <dialloalimou7@gmail.com>
2022-01-17 10:26:46 +0000

    commit 1

---
+++ src/main/java/com/aalim/menuprincpalussd/MenuprincpalussdApplication.java
@@ -0,0 +1,13 @@
+package com.aalim.menuprincpalussd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuprincpalussdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuprincpalussdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aalim/menuprincpalussd/MenuprincpalussdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aalim.menuprincpalussd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuprincpalussdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
